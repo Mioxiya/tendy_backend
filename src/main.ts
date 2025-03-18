d7f2fbdd753db968aa3d7d7dbdffd0ce08e317c5
Mioxiya <Mioxiya@gmail.com>
2025-03-18 22:51:27 +0800

    创建并使用全局异常过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 全局验证管道
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // 全局异常过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }
